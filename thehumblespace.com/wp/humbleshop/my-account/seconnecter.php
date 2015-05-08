11f7fa6e50064e1e9e7065c02edaf581b0f9f51c
thoussem <t.houssemg@gmail.com>
2015-05-08 20:24:55 +0100

    cc

---
+++ thehumblespace.com/wp/humbleshop/my-account/seconnecter.php
@@ -0,0 +1,32 @@
+<?php
+
+if(isset($_POST['seconnecter']))
+                {
+                // username and password sent from form 
+              
+                $username=$_POST['username'];
+				$password=$_POST['password'];
+                
+                
+                $sql="SELECT `id` FROM `utilisateurs` WHERE (`username`='$username' or `email`='$username') AND `password`='$password' ;";
+                $result=mysqli_query($conn,$sql);
+                $count=mysqli_num_rows($result);
+                
+                
+                // If result matched $myusername and $mypassword, table row must be 1 row
+                if($count==1)
+                {
+                
+                $_SESSION['login_user']=$username;
+                if(isset($_POST['rememberme'])){
+                	setcookie('auth',$username,time()+3600*24*3);
+                	}
+                header("location: ../homepage-with-carousel/index.php");
+                }
+                else 
+                {
+                $error="* Le nom d'Utilisateur ou le Mot de passe est Incorrecte";
+                   }
+            
+                }
+?>
